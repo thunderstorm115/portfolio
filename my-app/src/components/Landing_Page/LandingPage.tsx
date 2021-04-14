c6e2591de8405c9c1182ef20d337aefc70d39aab
Dennis Liebenberg <40003681+thunderstorm115@users.noreply.github.com>
2021-04-14 14:23:22 +0200

    update github

---
+++ my-app/src/components/Landing_Page/LandingPage.tsx
@@ -0,0 +1,18 @@
+import "./Styles.scss";
+
+ const Landing = () => {
+  return (
+    <div>
+      <h1 className="h1">Dennis Liebenberg</h1>
+      <h2>Developer</h2>
+      <h2>Minimalist</h2>
+      <h2>Poet</h2>
+      <h3>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua ut enim ad.Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit
+      </h3>
+    </div>
+  );
+};
+export default Landing;
