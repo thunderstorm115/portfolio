1d7728b7beb971c380fd62caf7e9095e0b570f2c
Dennis Liebenberg <40003681+thunderstorm115@users.noreply.github.com>
2021-04-14 16:23:34 +0200

    update github

--- my-app/src/components/Landing_Page/LandingPage.tsx
+++ my-app/src/components/Landing_Page/LandingPage.tsx
@@ -2,14 +2,14 @@ import "./Styles.scss";
 
  const Landing = () => {
   return (
-    <div>
+    <div className="grid">
       <h1 className="h1">Dennis Liebenberg</h1>
-      <h2>Developer</h2>
-      <h2>Minimalist</h2>
-      <h2>Poet</h2>
+      <h2>Developer </h2>
+      <h2> Minimalist </h2>
+      <h2> Poet</h2>
       <h3>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua ut enim ad.Lorem
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br/>
+        tempor incididunt ut labore et dolore magna aliqua ut enim ad.Lorem<br/>
         ipsum dolor sit amet, consectetur adipiscing elit
       </h3>
     </div>
